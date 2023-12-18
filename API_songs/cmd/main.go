e2fcd0a3de9fe10ec06f893cbde01f71dd9343c0
Abdenour Bounab <abdenour.bounab.cne@gmail.com>
2023-12-18 22:50:46 +0100

    adding the main in cmd with routes using chi.Router

---
+++ API_songs/cmd/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"songs/internal/controllers/songs"
+	"songs/internal/helpers"
+	_ "songs/internal/models"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Route("/songs", func(r chi.Router) {
+		r.Get("/", songs.GetSongs)
+		r.Route("/{id}", func(r chi.Router) {
+			r.Use(songs.Ctx)
+			r.Get("/", songs.GetSong)
+		})
+	})
+
+	logrus.Info("[INFO] Web server started. Now listening on *:8080")
+	logrus.Fatalln(http.ListenAndServe(":8080", r))
+}
+
+func init() {
+	db, err := helpers.OpenDB()
+	if err != nil {
+		logrus.Fatalf("error while opening database : %s", err.Error())
+	}
+	schemes := []string{
+		`CREATE TABLE IF NOT EXISTS songs (
+			song_id VARCHAR(255) PRIMARY KEY NOT NULL UNIQUE,
+			album VARCHAR(255) NOT NULL,
+    		artist VARCHAR(255) NOT NULL,
+    		song_name VARCHAR(255) NOT NULL,
+    		duration FLOAT NOT NULL,
+    		type_song VARCHAR(255) NOT NULL,
+    		playlist VARCHAR(255) NOT NULL
+		);`,
+	}
+	for _, scheme := range schemes {
+		if _, err := db.Exec(scheme); err != nil {
+			logrus.Fatalln("Could not generate table ! Error was : " + err.Error())
+		}
+	}
+	helpers.CloseDB(db)
+}
