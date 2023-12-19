b3c0b88b8912283e8fa7e5aebac85b57071ac47c
Abdenour Bounab <abdenour.bounab.cne@gmail.com>
2023-12-19 23:59:38 +0100

    adding POST controller

---
+++ API_songs/internal/controllers/songs/insert_song.go
@@ -0,0 +1,50 @@
+package songs
+
+import (
+	"encoding/json"
+	"github.com/gofrs/uuid"
+	"github.com/sirupsen/logrus"
+	"net/http"
+	"songs/internal/models"
+	"songs/internal/services/songs"
+)
+
+// GetCollection
+// @Tags         songs
+// @Summary      Get a collection.
+// @Description  Get a collection.
+// @Param        id           	path      string  true  "Collection UUID formatted ID"
+// @Success      200            {object}  models.Collection
+// @Failure      422            "Cannot parse id"
+// @Failure      500            "Something went wrong"
+// @Router       /songs/{id} [get]
+func InsertSong(w http.ResponseWriter, r *http.Request) {
+	var newSong models.Songs
+	err := json.NewDecoder(r.Body).Decode(&newSong)
+	if err != nil {
+		logrus.Errorf("Erreur lors de la lecture du corps de la requête : %s", err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	// Générer un nouvel ID UUID
+	id, err := uuid.NewV4()
+	if err != nil {
+		logrus.Errorf("Erreur lors de la génération de l'identifiant UUID : %s", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Affecter l'ID généré au commentaire
+	newSong.Id = &id
+
+	err = songs.InsertSong(newSong)
+	if err != nil {
+		logrus.Errorf("Erreur lors de la création du User : %s", err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	response, _ := json.Marshal(newSong)
+	_, _ = w.Write(response)
+}
