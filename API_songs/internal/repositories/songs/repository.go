b1e34ae6aa4bc4811b6255da6216c1f33af5eab7
Abdenour Bounab <abdenour.bounab.cne@gmail.com>
2023-12-20 15:16:16 +0100

    adding PUT functions in repository.go for songs

--- API_songs/internal/repositories/songs/repository.go
+++ API_songs/internal/repositories/songs/repository.go
@@ -67,3 +67,20 @@ func GetSongById(id uuid.UUID) (*models.Songs, error) {
 	}
 	return &song, err
 }
+
+func UpdateSong(song *models.Songs) error {
+	db, err := helpers.OpenDB()
+	if err != nil {
+		logrus.Errorf("Erreur lors de l'ouverture de la base de données : %s", err.Error())
+		return err
+	}
+	defer helpers.CloseDB(db)
+	_, err = db.Exec("UPDATE songs SET album=?, artist=?, song_name=?, duration=?, type_song=?, playlist=? WHERE id=?",
+		song.Id, song.Album, song.Artist, song.SongName, song.Duration, song.Type, song.Playlist)
+	if err != nil {
+		logrus.Errorf("Erreur lors de la mise à jour du user dans la base de données : %s", err.Error())
+		return err
+	}
+
+	return nil
+}
