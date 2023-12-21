0f4498d91f5077bbe5084ccf32b2d7558174087c
walidmer1688 <walid.mer1688@gmail.com>
2023-12-21 04:49:43 +0100

    Adding Get functions

---
+++ internal/models/models.go
@@ -0,0 +1,11 @@
+package models
+
+import (
+	"github.com/gofrs/uuid"
+)
+
+type User struct {
+	Id   *uuid.UUID `json:"id"`
+	Name string     `json:"name"`
+	Age  int        `json:"age"`
+}
---
+++ internal/services/users/service.go
@@ -0,0 +1,47 @@
+package users
+
+import (
+	"api_user/internal/models"
+	repository "api_user/internal/repositories/users"
+	"database/sql"
+	"errors"
+	"net/http"
+
+	"github.com/gofrs/uuid"
+	"github.com/sirupsen/logrus"
+)
+
+func GetAllUsers() ([]models.User, error) {
+	var err error
+	// calling repository
+	users, err := repository.GetAllUsers()
+	// managing errors
+	if err != nil {
+		logrus.Errorf("error retrieving users : %s", err.Error())
+		return nil, &models.CustomError{
+			Message: "Something went wrong",
+			Code:    500,
+		}
+	}
+
+	return users, nil
+}
+
+func GetUserById(id uuid.UUID) (*models.User, error) {
+	user, err := repository.GetUserById(id)
+	if err != nil {
+		if errors.As(err, &sql.ErrNoRows) {
+			return nil, &models.CustomError{
+				Message: "User not found",
+				Code:    http.StatusNotFound,
+			}
+		}
+		logrus.Errorf("error retrieving collections : %s", err.Error())
+		return nil, &models.CustomError{
+			Message: "Something went wrong",
+			Code:    500,
+		}
+	}
+
+	return user, err
+}
