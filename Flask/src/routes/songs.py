83865d8fb554183f47067e247e778dc2717d4791
Abdenour Bounab <abdenour.bounab.cne@gmail.com>
2024-01-10 20:59:23 +0100

    adding songs routes in flask

---
+++ Flask/src/routes/songs.py
@@ -0,0 +1,457 @@
+import json
+from flask import Blueprint, request
+from flask_login import login_required
+from models.http_exceptions import *
+from schemas.errors import *
+import services.songs as songs_service
+import services.ratings as ratings_service  
+# from routes import songs  
+songs = Blueprint(name="songs", import_name=__name__)
+
+
+
+@songs.route('/<song_id>/ratings', methods=['POST'])
+@login_required
+def post_rating(song_id):
+  
+    """
+    ---
+    post:
+      description: Creating a rating
+      parameters:
+        - in: path
+          name: song_id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of song id
+      requestBody:
+        required: true
+        content:
+            application/json:
+                schema: RatingCreate
+      responses:
+        '201':
+          description: Created
+          content:
+            application/json:
+              schema: Rating
+            application/yaml:
+              schema: Rating
+        '401':
+          description: Unauthorized
+          content:
+            application/json:
+              schema: Unauthorized
+            application/yaml:
+              schema: Unauthorized
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema: NotFound
+            application/yaml:
+              schema: NotFound
+        '422':
+          description: Unprocessable entity
+          content:
+            application/json:
+              schema: UnprocessableEntity
+            application/yaml:
+              schema: UnprocessableEntity
+      tags:
+          - ratings
+    """
+    print(request.json)
+    return ratings_service.create_rating(request.json, song_id)
+
+
+@songs.route('/<song_id>/ratings', methods=['GET'])
+@login_required
+def get_ratings(song_id):
+    """
+    ---
+    get:
+      description: Getting all ratings
+      parameters:
+        - in: path
+          name: song_id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of song id
+      responses:
+        '200':
+          description: Ok
+          content:
+            application/json:
+              schema: Ratings
+            application/yaml:
+              schema: Ratings
+        '401':
+          description: Unauthorized
+          content:
+            application/json:
+              schema: Unauthorized
+            application/yaml:
+              schema: Unauthorized
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema: NotFound
+            application/yaml:
+              schema: NotFound
+      tags:
+          - ratings
+    """
+    return ratings_service.get_ratings(song_id)
+
+
+@songs.route('/<song_id>/ratings/<id>', methods=['GET'])
+@login_required
+def get_rating(song_id, id):
+    """
+    ---
+    get:
+      description: Getting a rating
+      parameters:
+        - in: path
+          name: song_id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of song id
+        - in: path
+          name: id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of rating id
+      responses:
+        '200':
+          description: Ok
+          content:
+            application/json:
+              schema: Rating
+            application/yaml:
+              schema: Rating
+        '401':
+          description: Unauthorized
+          content:
+            application/json:
+              schema: Unauthorized
+            application/yaml:
+              schema: Unauthorized
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema: NotFound
+            application/yaml:
+              schema: NotFound
+      tags:
+          - ratings
+    """
+    return ratings_service.get_rating(song_id,id)
+
+
+@songs.route('/<song_id>/ratings/<id>', methods=['PUT'])
+@login_required
+def put_rating(song_id, id):
+    """
+    ---
+    put:
+      description: Updating a rating
+      parameters:
+        - in: path
+          name: song_id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of song id
+        - in: path
+          name: id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of rating id
+      requestBody:
+        required: true
+        content:
+            application/json:
+                schema: RatingUpdate
+      responses:
+        '200':
+          description: Ok
+          content:
+            application/json:
+              schema: Rating
+            application/yaml:
+              schema: Rating
+        '401':
+          description: Unauthorized
+          content:
+            application/json:
+              schema: Unauthorized
+            application/yaml:
+              schema: Unauthorized
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema: NotFound
+            application/yaml:
+              schema: NotFound
+        '422':
+          description: Unprocessable entity
+          content:
+            application/json:
+              schema: UnprocessableEntity
+            application/yaml:
+              schema: UnprocessableEntity
+      tags:
+          - ratings
+    """
+    return ratings_service.update_rating(song_id,id, request.json)
+
+
+@songs.route('/<song_id>/ratings/<id>', methods=['DELETE'])
+@login_required
+def delete_rating(song_id, id):
+    """
+    ---
+    delete:
+      description: Deleting a rating
+      parameters:
+        - in: path
+          name: song_id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of song id
+        - in: path
+          name: id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of rating id
+      responses:
+        '204':
+          description: No content
+        '401':
+          description: Unauthorized
+          content:
+            application/json:
+              schema: Unauthorized
+            application/yaml:
+              schema: Unauthorized
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema: NotFound
+            application/yaml:
+              schema: NotFound
+      tags:
+          - ratings
+    """
+    return ratings_service.delete_rating(song_id, id)
+
+
+
+@songs.route('/', methods=['GET'])
+@login_required
+def get_songs():
+    """
+    ---
+    get:
+      description: Getting all songs
+      responses:
+        '200':
+          description: Ok
+          content:
+            application/json:
+              schema: Songs
+            application/yaml:
+              schema: Songs
+        '401':
+
+          description: Unauthorized
+          content:
+            application/json:
+              schema: Unauthorized
+            application/yaml:
+              schema: Unauthorized
+    """
+    return songs_service.get_songs()
+
+@songs.route('/', methods=['POST'])
+@login_required
+def post_song():
+    """
+    ---
+    post:
+      description: Creating a song
+      requestBody:
+        required: true
+        content:
+            application/json:
+                schema: SongCreate
+      responses:
+        '201':
+          description: Created
+          content:
+            application/json:
+              schema: Song
+            application/yaml:
+              schema: Song
+        '401':
+          description: Unauthorized
+          content:
+            application/json:
+              schema: Unauthorized
+            application/yaml:
+              schema: Unauthorized
+        '422':
+          description: Unprocessable entity
+          content:
+            application/json:
+              schema: UnprocessableEntity
+            application/yaml:
+              schema: UnprocessableEntity
+      tags:
+          - songs
+    """
+    print(request.json, "request.json")
+    return songs_service.create_song(request.json)
+
+@songs.route('/<id>', methods=['GET'])
+@login_required
+def get_song(id):
+    """
+    ---
+    get:
+      description: Getting a song
+      parameters:
+        - in: path
+          name: id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of song id
+      responses:
+        '200':
+          description: Ok
+          content:
+            application/json:
+              schema: Song
+            application/yaml:
+              schema: Song
+        '401':
+          description: Unauthorized
+          content:
+            application/json:
+              schema: Unauthorized
+            application/yaml:
+              schema: Unauthorized
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema: NotFound
+            application/yaml:
+              schema: NotFound
+      tags:
+          - songs
+    """
+    return songs_service.get_song(id)
+
+@songs.route('/<id>', methods=['PUT'])
+@login_required
+def put_song(id):
+    """
+    ---
+    put:
+      description: Updating a song
+      parameters:
+        - in: path
+          name: id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of song id
+      requestBody:
+        required: true
+        content:
+            application/json:
+                schema: SongUpdate
+      responses:
+        '200':
+          description: Ok
+          content:
+            application/json:
+              schema: Song
+            application/yaml:
+              schema: Song
+        '401':
+          description: Unauthorized
+          content:
+            application/json:
+              schema: Unauthorized
+            application/yaml:
+              schema: Unauthorized
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema: NotFound
+            application/yaml:
+              schema: NotFound
+        '422':
+          description: Unprocessable entity
+          content:
+            application/json:
+              schema: UnprocessableEntity
+            application/yaml:
+              schema: UnprocessableEntity
+      tags:
+          - songs
+    """
+    return songs_service.update_song(id, request.json)
+
+@songs.route('/<id>', methods=['DELETE'])
+@login_required
+def delete_song(id):
+    """
+    ---
+    delete:
+      description: Deleting a song
+      parameters:
+        - in: path
+          name: id
+          schema:
+            type: uuidv4
+          required: true
+          description: UUID of song id
+      responses:
+        '204':
+          description: No content
+        '401':
+          description: Unauthorized
+          content:
+            application/json:
+              schema: Unauthorized
+            application/yaml:
+              schema: Unauthorized
+        '404':
+          description: Not found
+          content:
+            application/json:
+              schema: NotFound
+            application/yaml:
+              schema: NotFound
+      tags:
+          - songs
+    """
+    return songs_service.delete_song(id)
